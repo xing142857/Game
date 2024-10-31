aeb5fc1a6ef94db2dda6091bbcbdcdc622eaddf2
xing142857 <xing142857@gmail.com>
2024-10-31 14:36:13 +1100

    Update skeleton of the LifeGame

---
+++ game/src/pages/LifeGame.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LifeGame extends Component {
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
+
+export default LifeGame;
