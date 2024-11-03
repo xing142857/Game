ccdeb7813d12b50476980486dbbee91bc1f025d4
xing142857 <50036214+xing142857@users.noreply.github.com>
2024-11-03 14:03:56 +1100

    lol

--- game/src/pages/LifeGame.js
+++ game/src/pages/LifeGame.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import "../css/LifeGame.css";
+import { Container, Row, Col } from 'react-bootstrap';
 
-class LifeGame extends Component {
-    render() {
-        return (
-            <div>
-                Hello
-            </div>
-        );
-    }
+const LifeGame = () => {
+
+  // Initialize cell with a 100x100 grid filled with 0s
+  let cell = Array.from({ length: 100 }, () => Array(100).fill(0));
+
+
+  return (
+    <Container fluid>
+      {cell.map((row, rowIndex) => (
+        <Row key={rowIndex}>
+          {row.map((value, colIndex) => (
+            <Col key={colIndex} className="cell">
+              {value}
+            </Col>
+          ))}
+        </Row>
+      ))}
+    </Container>
+  )
 }
 
 export default LifeGame;
