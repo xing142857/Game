6d1329d016b188d8060127b78c69e530bedc8bc4
xing142857 <50036214+xing142857@users.noreply.github.com>
2024-11-18 20:27:46 +1100

    add Trump and Harris' image

--- game/src/pages/ElectionGame.js
+++ game/src/pages/ElectionGame.js
@@ -1,8 +1,10 @@
 import React, {  } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Image } from 'react-bootstrap';
 import ToolboxTheory from '../music/ToolboxTheory.mp3';
 import BurgundianLullaby from '../music/BurgundianLullaby.mp3';
 import { ReactComponent as BlankUSMap } from '../image/BlankUSMap.svg';
+import Trump from '../image/Trump.jpg'
+import Harris from '../image/Harris.jpg'
 
 const ElectionGame = () => {
 
@@ -11,12 +13,19 @@ const ElectionGame = () => {
         audio.play();
     }
 
-
     return (
         <>
-            <Button variant="primary" className='absolute top-3 text-xl' onClick={play_music}>Start</Button>
             <Container>
-            <div id="map-container">
+                <Container>
+                    <Button variant="primary" className='mt-3 text-xl' onClick={play_music}>Start</Button>
+                </Container>
+                <Container className='flex justify-between mt-10'>
+                    <Image alt="Trump" src={Trump} thumbnail rounded width="10%"/>
+                    <Image alt="Harris" src={Harris} thumbnail rounded width="10%"/>
+                </Container>
+            </Container>
+            <Container>
+            <div id="map-container" className='mt-10'>
                 <div id="usa-map">
                     <BlankUSMap />
                 </div>
