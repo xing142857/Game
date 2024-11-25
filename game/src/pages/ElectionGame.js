3c621e8a708a343a7797a9dc3a2612ef02abd9d0
xing142857 <xing142857@gmail.com>
2024-11-25 16:43:59 +1100

    swift photo

--- game/src/pages/ElectionGame.js
+++ game/src/pages/ElectionGame.js
@@ -23,8 +23,8 @@ const ElectionGame = () => {
                     <Button variant="primary" className='mt-3 text-xl' onClick={start_game}>Start</Button>
                 </Container>
                 <Container className='flex justify-between mt-10'>
-                    <Image alt="Trump" src={Trump} thumbnail width="10%"/>
                     <Image alt="Harris" src={Harris} thumbnail width="10%"/>
+                    <Image alt="Trump" src={Trump} thumbnail width="10%"/>
                 </Container>
             </Container>
             <Container>
