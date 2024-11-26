55a37d04b25eb28e758dba4d75079679a6f06965
xing142857 <50036214+xing142857@users.noreply.github.com>
2024-11-26 21:36:01 +1100

    Finish the score bar setting

--- game/src/pages/ElectionGame.js
+++ game/src/pages/ElectionGame.js
@@ -19,16 +19,16 @@ const ElectionGame = () => {
     return (
         <>
             <Container>
-                <Container>
-                    <Button variant="primary" className='mt-3 text-xl' onClick={start_game}>Start</Button>
-                </Container>
-                <Container className='flex justify-between mt-10'>
-                    <Image alt="Harris" src={Harris} thumbnail width="10%"/>
-                    <Image alt="Trump" src={Trump} thumbnail width="10%"/>
-                </Container>
+                <Button variant="primary" className='mt-3 text-xl' onClick={start_game}>Start</Button>
             </Container>
-            <Container>
-            <div id="map-container" className='mt-10'>
+            <Container className='w-full flex justify-center items-center mt-10'>
+                <Image className='w-1/12' alt="Harris" src={Harris} thumbnail/>
+                <div className='w-1 h-24 bg-stone-100 absolute left-1/2'></div>
+                <div className='w-6/12 h-20 bg-stone-500'></div>
+                <Image className='w-1/12' alt="Trump" src={Trump} thumbnail/>
+            </Container>
+            <Container className='w-full'>
+            <div id="map-container" className='mt-10 w-full'>
                 <div id="usa-map">
                     <BlankUSMap gameState={gameState} />
                 </div>
