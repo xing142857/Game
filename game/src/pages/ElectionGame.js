e629aac92f5cef780aa0bb32eeac684fe24fc3dc
xing142857 <xing142857@gmail.com>
2024-11-28 14:46:10 +1100

    Added colour bar (hasn't active)

--- game/src/pages/ElectionGame.js
+++ game/src/pages/ElectionGame.js
@@ -8,6 +8,60 @@ import Harris from '../image/Harris.jpg'
 
 const ElectionGame = () => {
 
+    const electionCollege = {
+        "ca": 54,
+        "ny": 28,
+        "il": 19,
+        "nj": 14,
+        "va": 13,
+        "wa": 12,
+        "ma": 11,
+        "co": 10,
+        "md": 10,
+        "mn": 10,
+        "or": 8,
+        "ct": 7,
+        "nm": 5,
+        "hi": 4,
+        "nh": 4,
+        "ri": 4,
+        "dc": 3,
+        "de": 3,
+        "vt": 3,
+        "me": 2,
+        "ne": 1,
+        "pa": 19,
+        "ga": 16,
+        "nc": 16,
+        "mi": 15,
+        "az": 11,
+        "wi": 10,
+        "nv": 6,
+        "tx": 40,
+        "fl": 30,
+        "oh": 17,
+        "in": 11,
+        "tn": 11,
+        "mo": 10,
+        "al": 9,
+        "sc": 9,
+        "ky": 8,
+        "la": 8,
+        "ok": 7,
+        "ar": 6,
+        "ia": 6,
+        "ks": 6,
+        "ms": 6,
+        "ut": 6,
+        "id": 4,
+        "mt": 4,
+        "wv": 4,
+        "ak": 3,
+        "nd": 3,
+        "sd": 3,
+        "wy": 3,
+      };
+
     const [gameState, setGameState] = useState(false);
 
     const start_game = () => {
@@ -23,8 +77,10 @@ const ElectionGame = () => {
             </Container>
             <Container className='w-full flex justify-center items-center mt-10'>
                 <Image className='w-1/12' alt="Harris" src={Harris} thumbnail/>
-                <div className='w-1 h-24 bg-stone-100 absolute left-1/2'></div>
+                <div className={`h-20 left-1/4 bg-democratic absolute`}></div>
                 <div className='w-6/12 h-20 bg-stone-500'></div>
+                <div className={`h-20 right-1/4 bg-republican absolute`}></div>
+                <div className='w-1 h-24 bg-stone-100 absolute left-1/2'></div>
                 <Image className='w-1/12' alt="Trump" src={Trump} thumbnail/>
             </Container>
             <Container className='w-full'>
--- game/tailwind.config.js
+++ game/tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         '60': 'repeat(60, minmax(0, 1fr))',
         '70': 'repeat(70, minmax(0, 1fr))',
         '100': 'repeat(100, minmax(0, 1fr))',
+      },
+      colors: {
+        'republican': '#610a0d',
+        'democratic': '#00405b',
       }
     }
   },
