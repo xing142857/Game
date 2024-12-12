5cf891913a4968e7c1ed0dcb5ae207347c68ca32
xing142857 <50036214+xing142857@users.noreply.github.com>
2024-12-12 20:09:55 +1100

    tep save

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,6 +5,7 @@ const msg = ref('Hello World!')
 </script>
 
 <template>
+  <div class="w-3 h-3 bg-sky-500"></div>
   <h1>{{ msg }}</h1>
   <input v-model="msg" />
 </template>
--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/App.css'
 
 createApp(App).mount('#app')
